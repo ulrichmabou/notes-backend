aec95b3c64e3e6b299b52e81d34eba7516b66f3a
UlrichMabou <ulrichmabou@gmail.com>
2020-11-09 19:20:19 +0200

    add production build

---
+++ build/precache-manifest.75dac7bce642b58e4f8ea995e13f773a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb14cec56c1af959bfd0d018fd1b9dd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0ac2b9384ce2050f1f2",
+    "url": "/static/css/main.07da5f1e.chunk.css"
+  },
+  {
+    "revision": "b687281982d0621163a2",
+    "url": "/static/js/2.590b4e07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.590b4e07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ac2b9384ce2050f1f2",
+    "url": "/static/js/main.eaf11842.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
